8fbcb69b1500729926d233edf8cd98ef9823e8e2
ericjt505 <weirdspacetobe@gmail.com>
2020-08-12 15:26:58 -0400

    update @ 3:26pm

--- scripts.js
+++ scripts.js
@@ -19,15 +19,47 @@ console.log(stringOne.endsWith("to"))
 console.log(String.fromCharCode(114))
 
 // includes()
-
+console.log(stringTwo.includes("end"))
 
 // indexOf()
-
+console.log(stringTwo.indexOf("end"))
 
 // lastIndexOf()
-
+console.log(stringTwo.lastIndexOf("end"))
 
 // match()
-
+console.log(stringTwo.match(/end/g))
 
 // repeat()
+console.log(stringOne.repeat(3))
+
+// replace()
+console.log(stringTwo.replace(/end/g, "END"))
+
+// search()
+console.log(stringTwo.search("end"))
+
+// slice()
+console.log(stringTwo.slice(2, 4))
+
+// split()
+console.log(stringOne.split(" "))
+
+// startsWith()
+console.log(stringOne.startsWith("free"))
+
+// substr()
+console.log(stringTwo.substr(2, 4))
+
+// substring()
+console.log(stringTwo.substring(2, 4))
+
+// tolowerCase()
+console.log(stringOne.toLowerCase())
+
+// toUpperCase()
+console.log(stringOne.toUpperCase())
+
+// trim()
+var stringThree = "     Subscribe now!       "
+console.log(stringThree.trim())
