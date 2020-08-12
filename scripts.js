7454fbc29eeabcad7f895e5deb08b9cc944ef1ac
ericjt505 <weirdspacetobe@gmail.com>
2020-08-12 15:11:12 -0400

    update

---
+++ scripts.js
@@ -0,0 +1,33 @@
+/* 20 String Methods */
+
+var stringOne = "freeCodeComp is the best place to learn"
+var stringTwo = "frontend and backend development"
+
+// charAt()
+console.log(stringOne.charAt(1))
+
+// charCodeAt()
+console.log(stringOne.charCodeAt(1))
+
+// concat()
+console.log(stringOne.concat(stringTwo))
+
+// endsWith()
+console.log(stringOne.endsWith("to"))
+
+// fromCharCode()
+console.log(String.fromCharCode(114))
+
+// includes()
+
+
+// indexOf()
+
+
+// lastIndexOf()
+
+
+// match()
+
+
+// repeat()
